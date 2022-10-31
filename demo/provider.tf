a01d030663b127d71f6b97426a117314d0d0dcef
prudhviraj45 <115442733+prudhviraj45@users.noreply.github.com>
2022-10-31 09:19:13 +0530

    Update provider.tf

--- demo/provider.tf
+++ demo/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 
     region = "ap-south-1"
-    access_key = "AKIAQTRS3MZZPVGA7QNF"
-    secret_key = "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"
+    access_key = "access_key"
+    secret_key = "secret_key"
 }
